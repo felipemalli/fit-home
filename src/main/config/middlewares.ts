3176a122e22436e01a18c3a917803f2fe1b86f76
felipemalli <felipevahia@gmail.com>
2023-03-18 17:24:58 -0300

    refactor: group middleware interfaces in a single file

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,5 @@
 import { Express } from 'express'
-import { contentType } from '../middlewares/content-type'
-import { cors } from '../middlewares/cors'
-import { json } from '../middlewares/json'
+import { contentType, cors, json } from '../middlewares'
 
 export default (app: Express): void => {
   app.use(json)
