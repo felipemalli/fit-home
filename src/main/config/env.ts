c4ee7b8e3b5af85d082ca3555dc0c4faf86e5245
felipemalli <felipevahia@gmail.com>
2023-04-05 23:31:14 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://mongo:27017/fit-home-api',
-  port: process.env.PORT ?? 5050
+  port: process.env.PORT ?? 5050,
+  jwtSecret: process.env.JWT_SECRET ?? 'aps2-#0'
 }
