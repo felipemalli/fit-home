7e9c3ccea7ab3740e18f01f4b5bd2e24cfe82fcf
felipemalli <felipevahia@gmail.com>
2023-04-07 17:58:08 -0300

    feat: ensure AddExerciseController returns a bad request error if validation fails

--- src/presentation/controllers/exercise/add-exercise/add-exercise-controller.ts
+++ src/presentation/controllers/exercise/add-exercise/add-exercise-controller.ts
@@ -1,3 +1,4 @@
+import { badRequest } from '../../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, Validation } from './add-exercise-controller-protocols'
 
 export class AddExerciseController implements Controller {
@@ -6,7 +7,10 @@ export class AddExerciseController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
+    const error = this.validation.validate(httpRequest.body)
+    if (error) {
+      return badRequest(error)
+    }
     return await new Promise(resolve => resolve({
       statusCode: 404,
       body: 'Not implemented'
