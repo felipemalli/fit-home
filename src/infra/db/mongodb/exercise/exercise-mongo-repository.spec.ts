e1164242596469fb7da90e6244f7dc98467141f7
felipemalli <felipevahia@gmail.com>
2023-04-08 04:03:17 -0300

    test: ensure ExerciseMongoRepository return an exercise on add success

---
+++ src/infra/db/mongodb/exercise/exercise-mongo-repository.spec.ts
@@ -0,0 +1,42 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { ExerciseMongoRepository } from './exercise-mongo-repository'
+
+let exerciseCollection: Collection
+
+describe('Exercise Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    exerciseCollection = await MongoHelper.getCollection('exercises')
+    await exerciseCollection.deleteMany({})
+  })
+
+  const makeSut = (): ExerciseMongoRepository => {
+    return new ExerciseMongoRepository()
+  }
+
+  it('Should return an exercise on add success', async () => {
+    const sut = makeSut()
+    const account = await sut.add({
+      name: 'any_name',
+      series: 1,
+      betweenSeriesTime: 120,
+      repetitions: 12,
+      repetitionTime: 4.5
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.series).toBe(1)
+    expect(account.betweenSeriesTime).toBe(120)
+    expect(account.repetitions).toBe(12)
+    expect(account.repetitionTime).toBe(4.5)
+  })
+})
