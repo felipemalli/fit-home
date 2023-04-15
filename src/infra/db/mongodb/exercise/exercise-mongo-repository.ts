81142d1048e959f55df86c911fbd48c87c6ff63c
felipemalli <felipevahia@gmail.com>
2023-04-14 22:32:35 -0300

    feat: add new properties of exercise model to infra

--- src/infra/db/mongodb/exercise/exercise-mongo-repository.ts
+++ src/infra/db/mongodb/exercise/exercise-mongo-repository.ts
@@ -6,7 +6,15 @@ import { MongoHelper } from '../helpers/mongo-helper'
 export class ExerciseMongoRepository implements AddExerciseRepository {
   async add (exerciseData: AddExerciseModel): Promise<ExerciseModel> {
     const exerciseCollection = await MongoHelper.getCollection('exercises')
-    const { insertedId } = await exerciseCollection.insertOne(exerciseData)
+    const { configuration, ...data } = exerciseData
+    const exerciseModel: Omit<ExerciseModel, 'id'> = {
+      ...data,
+      description: data.description ?? '',
+      url: data.url ?? '',
+      configurations: [configuration],
+      isFavorite: false
+    }
+    const { insertedId } = await exerciseCollection.insertOne(exerciseModel)
     const exercise = await exerciseCollection.findOne({ _id: insertedId })
     return MongoHelper.map(exercise)
   }
