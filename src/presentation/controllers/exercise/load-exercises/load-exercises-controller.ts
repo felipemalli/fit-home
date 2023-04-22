8549843ecee8667aef2b4ed1f25cdcac1ea96f7a
felipemalli <felipevahia@gmail.com>
2023-04-21 23:00:12 -0300

    feat: ensure LoadExercisesController returns 500 on throws

--- src/presentation/controllers/exercise/load-exercises/load-exercises-controller.ts
+++ src/presentation/controllers/exercise/load-exercises/load-exercises-controller.ts
@@ -1,11 +1,15 @@
-import { ok } from '../../../helpers/http/http-helper'
+import { ok, serverError } from '../../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadExercises } from './load-exercises-protocols'
 
 export class LoadExercisesController implements Controller {
   constructor (private readonly loadExercises: LoadExercises) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const exercises = await this.loadExercises.load()
-    return ok(exercises)
+    try {
+      const exercises = await this.loadExercises.load()
+      return ok(exercises)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
