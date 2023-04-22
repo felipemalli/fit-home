0aea7d954884d4886de8f350c677629d2cb5158e
felipemalli <felipevahia@gmail.com>
2023-04-22 20:33:26 -0300

    test: ensure DbLoadExercises calls LoadExercisesRepository

---
+++ src/data/usecases/load-exercises/db-load-exercises.spec.ts
@@ -0,0 +1,59 @@
+import { ExerciseModel } from '../../../domain/models/exercises/exercise'
+import { LoadExercisesRepository } from '../../protocols/db/exercise/load-exercises-repository'
+import { DbLoadExercises } from './db-load-exercises'
+
+const makeFakeExercises = (): ExerciseModel[] => {
+  return [{
+    id: 'any_id',
+    name: 'any_name',
+    description: 'any_description',
+    workoutId: 'any_workout_id',
+    templateId: 'any_template_id',
+    accountId: 'any_account_id',
+    selectedVariationId: 'any_variation_id',
+    variations: [{
+      id: 'any_variation_id',
+      name: 'any_variation_name',
+      description: 'any_variation_description',
+      url: 'https://www.any_variation_url.com/',
+      configuration: {
+        series: 1,
+        betweenSeriesTime: 120,
+        repetitions: 12,
+        repetitionTime: 4.5,
+        warmupTime: 0,
+        weight: 10
+      }
+    }]
+  }, {
+    id: 'other_id',
+    name: 'other_name',
+    accountId: 'other_account_id',
+    selectedVariationId: 'other_variation_id',
+    variations: [{
+      id: 'other_variation_id',
+      name: 'other_variation_name',
+      configuration: {
+        series: 3,
+        betweenSeriesTime: 70,
+        repetitions: 8,
+        repetitionTime: 3.8
+      }
+    }]
+  }]
+}
+
+describe('DbLoadExercises', () => {
+  it('Should call LoadExercises with correct values', async () => {
+    class LoadExercisesRepositoryStub implements LoadExercisesRepository {
+      async loadAll (accountId: string): Promise<ExerciseModel[]> {
+        return await new Promise(resolve => resolve(makeFakeExercises()))
+      }
+    }
+    const loadExercisesRepositoryStub = new LoadExercisesRepositoryStub()
+    const loadAllSpy = jest.spyOn(loadExercisesRepositoryStub, 'loadAll')
+    const sut = new DbLoadExercises(loadExercisesRepositoryStub)
+    await sut.load('any_id')
+    expect(loadAllSpy).toHaveBeenCalledWith('any_id')
+  })
+})
