235fecef47e33ade320a1ab0f753123fffbfab7a
felipemalli <felipevahia@gmail.com>
2023-04-23 01:28:56 -0300

    refactor: move auth middlewares to separate files

--- src/main/routes/exercise-routes.ts
+++ src/main/routes/exercise-routes.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express'
-import { adaptMiddleware } from '../adapters/express-middleware-adapter'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddExerciseController } from '../factories/controllers/exercise/add-exercise/add-exercise-controller-factory'
 import { makeLoadExercisesController } from '../factories/controllers/exercise/load-exercises/load-exercises-controller-factory'
-import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
+import { auth } from '../middlewares/auth'
 
 export default (router: Router): void => {
-  const userAuth = adaptMiddleware(makeAuthMiddleware())
-  router.post('/exercises', userAuth, adaptRoute(makeAddExerciseController()))
-  router.get('/exercises', userAuth, adaptRoute(makeLoadExercisesController()))
+  router.post('/exercises', auth, adaptRoute(makeAddExerciseController()))
+  router.get('/exercises', auth, adaptRoute(makeLoadExercisesController()))
 }
