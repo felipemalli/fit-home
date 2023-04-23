5afaa99a56b9e3671fdf128bc44d16751bfe71c0
felipemalli <felipevahia@gmail.com>
2023-04-22 22:18:20 -0300

    test: ensure ExerciseMongoRepository load all exercises

--- src/infra/db/mongodb/exercise/exercise-mongo-repository.spec.ts
+++ src/infra/db/mongodb/exercise/exercise-mongo-repository.spec.ts
@@ -1,4 +1,4 @@
-import { Collection } from 'mongodb'
+import { Collection, ObjectId } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { ExerciseMongoRepository } from './exercise-mongo-repository'
 
@@ -59,6 +59,73 @@ describe('Exercise Mongo Repository', () => {
       expect(firstVariation.configuration.repetitionTime).toBe(4.5)
       expect(firstVariation.configuration.warmupTime).toBe(0)
       expect(firstVariation.configuration.weight).toBe(10)
+    })
+  })
+
+  describe('loadAll()', () => {
+    it('Should load all exercises by account on success', async () => {
+      const firstVariationId = new ObjectId()
+      const secondVariationId = new ObjectId()
+      await exerciseCollection.insertMany([{
+        _id: new ObjectId(),
+        name: 'any_name',
+        description: 'any_description',
+        workoutId: 'any_workout_id',
+        templateId: 'any_template_id',
+        accountId: 'same_account_id',
+        selectedVariationId: firstVariationId,
+        variations: [{
+          _id: firstVariationId,
+          name: 'any_variation_name',
+          description: 'any_variation_description',
+          url: 'https://www.any_variation_url.com/',
+          configuration: {
+            series: 1,
+            betweenSeriesTime: 120,
+            repetitions: 12,
+            repetitionTime: 4.5,
+            warmupTime: 0,
+            weight: 10
+          }
+        }]
+      }, {
+        _id: new ObjectId(),
+        name: 'other_name',
+        accountId: 'same_account_id',
+        selectedVariationId: secondVariationId,
+        variations: [{
+          _id: secondVariationId,
+          name: 'other_variation_name',
+          configuration: {
+            series: 3,
+            betweenSeriesTime: 70,
+            repetitions: 8,
+            repetitionTime: 3.8
+          }
+        }]
+      }])
+      const sut = makeSut()
+      const accounts = await sut.loadAll('same_account_id')
+      expect(accounts.length).toBe(2)
+      expect(accounts[0]).toBeTruthy()
+      expect(accounts[0].id).toBeTruthy()
+      expect(accounts[0].name).toBe('any_name')
+      expect(accounts[0].description).toBe('any_description')
+      expect(accounts[0].workoutId).toBe('any_workout_id')
+      expect(accounts[0].templateId).toBe('any_template_id')
+      expect(accounts[0].selectedVariationId).toBeTruthy()
+      const { variations: [firstVariation] } = accounts[0]
+      expect(accounts[0].selectedVariationId.toString()).toBe(firstVariation.id)
+      expect(firstVariation.name).toBe('any_variation_name')
+      expect(firstVariation.description).toBe('any_variation_description')
+      expect(firstVariation.url).toBe('https://www.any_variation_url.com/')
+      expect(firstVariation.configuration.series).toBe(1)
+      expect(firstVariation.configuration.betweenSeriesTime).toBe(120)
+      expect(firstVariation.configuration.repetitions).toBe(12)
+      expect(firstVariation.configuration.repetitionTime).toBe(4.5)
+      expect(firstVariation.configuration.warmupTime).toBe(0)
+      expect(firstVariation.configuration.weight).toBe(10)
+      expect(accounts[1].name).toBe('other_name')
     })
   })
 })
