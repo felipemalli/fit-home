f8c25aa6b9baf23b936886b3a888d2a4b7be0547
felipemalli <felipevahia@gmail.com>
2023-05-17 02:55:52 -0300

    test: ensure DbUpdateExercise calls UpdateExerciseRepository with correct values

---
+++ src/data/usecases/update-exercise/db-update-exercise.spec.ts
@@ -0,0 +1,61 @@
+
+import { DbUpdateExercise } from './db-update-exercise'
+import { UpdateExerciseModel, ExerciseModel, UpdateExerciseRepository } from './db-update-exercise-protocols'
+
+const makeFakeExerciseData = (): UpdateExerciseModel => ({
+  name: 'updated_name',
+  description: 'updated_description',
+  isTemplate: true
+})
+
+const makeFakeExercise = (): ExerciseModel => Object.assign({}, makeFakeExerciseData() as Required<UpdateExerciseModel>, {
+  id: 'any_id',
+  accountId: 'any_account_id',
+  variations: [{
+    id: 'any_variation_id',
+    name: 'any_variation_name',
+    description: 'any_variation_description',
+    url: 'https://www.any_variation_url.com/',
+    configuration: {
+      series: 1,
+      betweenSeriesTime: 120,
+      repetitions: 12,
+      repetitionTime: 4.5,
+      warmupTime: 0,
+      weight: 10
+    }
+  }]
+})
+
+const makeUpdateExerciseRepository = (): UpdateExerciseRepository => {
+  class UpdateExerciseRepositoryStub implements UpdateExerciseRepository {
+    async update (data: UpdateExerciseModel): Promise<ExerciseModel> {
+      return await new Promise(resolve => resolve(makeFakeExercise()))
+    }
+  }
+  return new UpdateExerciseRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbUpdateExercise
+  updateExerciseRepositoryStub: UpdateExerciseRepository
+}
+
+const makeSut = (): SutTypes => {
+  const updateExerciseRepositoryStub = makeUpdateExerciseRepository()
+  const sut = new DbUpdateExercise(updateExerciseRepositoryStub)
+  return {
+    sut,
+    updateExerciseRepositoryStub
+  }
+}
+
+describe('DbUpdateExercise UseCase', () => {
+  it('Should call UpdateExerciseRepository with correct values', async () => {
+    const { sut, updateExerciseRepositoryStub } = makeSut()
+    const updateSpy = jest.spyOn(updateExerciseRepositoryStub, 'update')
+    const exerciseData = makeFakeExerciseData()
+    await sut.update(exerciseData)
+    expect(updateSpy).toHaveBeenCalledWith(exerciseData)
+  })
+})
