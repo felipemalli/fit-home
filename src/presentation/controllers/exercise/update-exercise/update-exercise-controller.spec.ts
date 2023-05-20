f5a6de758127c63b8decb002ec849cb89198edbb
felipemalli <felipevahia@gmail.com>
2023-05-20 04:30:19 -0300

    test: ensure UpdateExerciseController returns 200 on success

--- src/presentation/controllers/exercise/update-exercise/update-exercise-controller.spec.ts
+++ src/presentation/controllers/exercise/update-exercise/update-exercise-controller.spec.ts
@@ -1,13 +1,13 @@
 import { UpdateExerciseController } from './update-exercise-controller'
 import { ExerciseModel, HttpRequest, LoadExerciseById, UpdateExercise, UpdateExerciseBodyModel, UpdateExerciseParamsModel } from './update-exercise-controller-protocols'
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
+import { forbidden, ok, serverError } from '@/presentation/helpers/http/http-helper'
 
 const makeFakeRequest = (): HttpRequest<UpdateExerciseBodyModel, UpdateExerciseParamsModel> => ({
   body: {
     name: 'updated_name',
     description: 'updated_description',
-    isTemplate: true
+    isTemplate: false
   },
   params: {
     exerciseId: 'any_exercise_id'
@@ -36,6 +36,28 @@ const makeFakeExercise = (): ExerciseModel => ({
   }]
 })
 
+const makeFakeUpdatedExercise = (): ExerciseModel => ({
+  id: 'any_id',
+  name: 'updated_name',
+  description: 'updated_description',
+  accountId: 'any_account_id',
+  isTemplate: false,
+  variations: [{
+    id: 'any_variation_id',
+    name: 'any_variation_name',
+    description: 'any_variation_description',
+    url: 'https://www.any_variation_url.com/',
+    configuration: {
+      series: 1,
+      betweenSeriesTime: 120,
+      repetitions: 12,
+      repetitionTime: 4.5,
+      warmupTime: 0,
+      weight: 10
+    }
+  }]
+})
+
 const makeLoadExerciseById = (): LoadExerciseById => {
   class LoadExerciseByIdStub implements LoadExerciseById {
     async loadById (id: string): Promise<ExerciseModel | null> {
@@ -48,7 +70,7 @@ const makeLoadExerciseById = (): LoadExerciseById => {
 const makeUpdateExercise = (): UpdateExercise => {
   class UpdateExerciseStub implements UpdateExercise {
     async update (id: string, data: UpdateExerciseBodyModel): Promise<ExerciseModel> {
-      return await new Promise(resolve => resolve(makeFakeExercise()))
+      return await new Promise(resolve => resolve(makeFakeUpdatedExercise()))
     }
   }
   return new UpdateExerciseStub()
@@ -108,5 +130,11 @@ describe('UpdateExercise Controller', () => {
     jest.spyOn(updateExerciseStub, 'update').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  it('Should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(ok(makeFakeUpdatedExercise()))
   })
 })
