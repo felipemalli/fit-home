7a6bba85d33776b03ab5fc488ce8ddaca87f419f
felipemalli <felipevahia@gmail.com>
2023-05-21 03:25:28 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Fit Home API',
+    description: 'API for handling physical exercises organization and improve their efficiency',
+    version: '1.0.0'
+  }
+}
