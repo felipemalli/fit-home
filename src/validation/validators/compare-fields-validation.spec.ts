5c71e61ef1f1232df095494142da21c954d50661
felipemalli <felipevahia@gmail.com>
2023-05-31 02:45:18 -0300

    refactor: add more flexibility with constants to tests of validation classes

--- src/validation/validators/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields-validation.spec.ts
@@ -2,25 +2,29 @@
 import { CompareFieldsValidation } from './compare-fields-validation'
 import { InvalidParamError } from '@/presentation/errors'
 
+const field = 'any_field'
+const fieldToCompare = 'any_other_field'
+const value = 'any_value'
+
 const makeSut = (): CompareFieldsValidation => {
-  return new CompareFieldsValidation('field', 'fieldToCompare')
+  return new CompareFieldsValidation(field, fieldToCompare)
 }
 
 describe('CompareFields Validation', () => {
   it('Should return a InvalidParamError if validation fails', () => {
     const sut = makeSut()
     const error = sut.validate({
-      field: 'any_value',
-      fieldToCompare: 'different_value'
+      [field]: value,
+      [fieldToCompare]: 'other_value'
     })
-    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+    expect(error).toEqual(new InvalidParamError(fieldToCompare))
   })
 
   it('Should not return if validation succeeds', () => {
     const sut = makeSut()
     const error = sut.validate({
-      field: 'any_value',
-      fieldToCompare: 'any_value'
+      [field]: value,
+      [fieldToCompare]: value
     })
     expect(error).toBeUndefined()
   })
--- src/validation/validators/required-field-validation.spec.ts
+++ src/validation/validators/required-field-validation.spec.ts
@@ -2,20 +2,23 @@
 import { RequiredFieldValidation } from './required-field-validation'
 import { MissingParamError } from '@/presentation/errors'
 
+const field = 'any_field'
+const value = 'any_value'
+
 const makeSut = (): RequiredFieldValidation => {
-  return new RequiredFieldValidation('field')
+  return new RequiredFieldValidation(field)
 }
 
 describe('RequiredField Validation', () => {
   it('Should return a MissingParamError if validation fails', () => {
     const sut = makeSut()
-    const error = sut.validate({ name: 'any_name' })
-    expect(error).toEqual(new MissingParamError('field'))
+    const error = sut.validate({ invalidField: value })
+    expect(error).toEqual(new MissingParamError(field))
   })
 
   it('Should not return if validation succeeds', () => {
     const sut = makeSut()
-    const error = sut.validate({ field: 'any_field' })
+    const error = sut.validate({ [field]: value })
     expect(error).toBeUndefined()
   })
 })
